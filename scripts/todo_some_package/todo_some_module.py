06c6af5e264b435f0a74c705d2cad5eab177ec4f
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-06-10 15:10:36 -0400

    No need for this docstring example in code, as it is in the docs

--- scripts/todo_some_package/todo_some_module.py
+++ scripts/todo_some_package/todo_some_module.py
@@ -25,32 +25,3 @@ def print_some_func_ret(arg1: int) -> None:
 # TODO: Disable mandatory docstring for private functions
 def _some_func() -> str:
     return 'test'
-
-    # Example of Google-style docstring
-
-    """Summary line.
-
-    Extended description of function.
-
-    Args:
-        arg1 (int): Description of arg1
-        arg2 (str): Description of arg2
-
-    Returns:
-        bool: Description of return value
-
-    Raises:
-        AttributeError: The ``Raises`` section is a list of all exceptions
-            that are relevant to the interface.
-        ValueError: If `arg2` is equal to `arg1`.
-
-    Examples:
-        Examples should be written in doctest format, and should illustrate how
-        to use the function.
-
-        >>> a=1
-        >>> b=2
-        >>> func(a,b)
-        True
-
-    """
