5cebfcf3dcedeae90b53aed44878f032ccb08dc7
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-06-13 15:36:53 -0400

    Fixed docstring ref

--- fcg/config/voxsim/world.py
+++ fcg/config/voxsim/world.py
@@ -7,6 +7,6 @@ class World:
     """Configuration wrapper for the initial parameters of
     :py:func:`simulator.factory.geometry_factory.handlers.geometry_handler.GeometryHandler`.
 
-    See :py:func:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory.get_geometry_handler`
+    See :py:meth:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory.get_geometry_handler`
     """
     resolution: typing.Tuple[int, int, int]
