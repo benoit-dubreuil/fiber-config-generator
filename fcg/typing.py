0a732b0126cbe2b3f8e7f67f9f7b0f439900e767
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-06-13 16:18:35 -0400

    Added Vector3 types

--- fcg/typing.py
+++ fcg/typing.py
@@ -1,3 +1,7 @@
 import typing
 
 Numeric = typing.TypeVar('Numeric', int, float)
+
+Vector3i = typing.Tuple[int, int, int]
+Vector3f = typing.Tuple[float, float, float]
+Vector3 = typing.Tuple[Numeric, Numeric, Numeric]
