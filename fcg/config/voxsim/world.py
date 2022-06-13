1faf0222f4741e261a03a706c73966b013883581
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-06-13 15:30:04 -0400

    Added class doc

--- fcg/config/voxsim/world.py
+++ fcg/config/voxsim/world.py
@@ -4,4 +4,9 @@ import dataclasses
 
 @dataclasses.dataclass(frozen=True)
 class World:
+    """Configuration wrapper for the initial parameters of
+    :py:func:`simulator.factory.geometry_factory.handlers.geometry_handler.GeometryHandler`.
+
+    See :py:func:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory.get_geometry_handler`
+    """
     resolution: typing.Tuple[int, int, int]
