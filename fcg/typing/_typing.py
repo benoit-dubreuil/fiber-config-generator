5b77dc71a8ba8eb4acf58945f135f67d770369a5
benoit-dubreuil <6900372+benoit-dubreuil@users.noreply.github.com>
2022-06-14 17:05:51 -0400

    Added Vec2 types

--- fcg/typing/_typing.py
+++ fcg/typing/_typing.py
@@ -4,6 +4,10 @@ Numeric = typing.Union[int, float]
 
 T_Numeric = typing.TypeVar('T_Numeric', int, float)
 
+Vec2 = typing.Tuple[T_Numeric, T_Numeric]
+Vec2i = Vec2[int]
+Vec2f = Vec2[float]
+
 Vec3 = typing.Tuple[T_Numeric, T_Numeric, T_Numeric]
 Vec3i = Vec3[int]
 Vec3f = Vec3[float]
