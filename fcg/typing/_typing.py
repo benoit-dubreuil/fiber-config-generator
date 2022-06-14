77bbe318dbc3092ae268b8917a5a14a0bf4d1510
benoit-dubreuil <6900372+benoit-dubreuil@users.noreply.github.com>
2022-06-14 17:18:35 -0400

    Renamed to match PEP 8 style Guide for Python Code

--- fcg/typing/_typing.py
+++ fcg/typing/_typing.py
@@ -2,12 +2,12 @@ import typing
 
 Numeric = typing.Union[int, float]
 
-T_Numeric = typing.TypeVar('T_Numeric', int, float)
+T_numeric = typing.TypeVar('T_numeric', int, float)
 
-Vec2 = typing.Tuple[T_Numeric, T_Numeric]
+Vec2 = typing.Tuple[T_numeric, T_numeric]
 Vec2i = Vec2[int]
 Vec2f = Vec2[float]
 
-Vec3 = typing.Tuple[T_Numeric, T_Numeric, T_Numeric]
+Vec3 = typing.Tuple[T_numeric, T_numeric, T_numeric]
 Vec3i = Vec3[int]
 Vec3f = Vec3[float]
