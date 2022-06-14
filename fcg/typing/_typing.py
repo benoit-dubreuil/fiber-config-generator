2a8631b76a985440f8119267d0096bd00948b578
benoit-dubreuil <6900372+benoit-dubreuil@users.noreply.github.com>
2022-06-14 16:57:04 -0400

    Renamed Numeric

--- fcg/typing/_typing.py
+++ fcg/typing/_typing.py
@@ -1,7 +1,7 @@
 import typing
 
-Numeric = typing.TypeVar('Numeric', int, float)
+T_Numeric = typing.TypeVar('T_Numeric', int, float)
 
-Vector3 = typing.Tuple[Numeric, Numeric, Numeric]
+Vector3 = typing.Tuple[T_Numeric, T_Numeric, T_Numeric]
 Vector3i = Vector3[int]
 Vector3f = Vector3[float]
