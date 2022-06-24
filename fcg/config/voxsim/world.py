d8d7cb01552d3c430e298c83f53f9b663c343427
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-06-24 11:47:48 -0400

    Removed superfluous :py: ref

--- fcg/config/voxsim/world.py
+++ fcg/config/voxsim/world.py
@@ -6,9 +6,9 @@ import fcg.typing
 @dataclasses.dataclass(frozen=True)
 class World:
     """Configuration wrapper for the initial parameters of
-    :py:func:`simulator.factory.geometry_factory.handlers.geometry_handler.GeometryHandler`.
+    :func:`simulator.factory.geometry_factory.handlers.geometry_handler.GeometryHandler`.
 
-    See :py:meth:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory.get_geometry_handler`
+    See :meth:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory.get_geometry_handler`
     """
 
     resolution: fcg.typing.Vec3i
