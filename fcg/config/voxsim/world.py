da884afdedde02b333b7a155255e2ded01cc543c
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-06-24 11:54:47 -0400

    Revert "Added attributes doc"
    This reverts commit 36cb00ddf1c577a709c78f3c6818468b22a9f5a9.

--- fcg/config/voxsim/world.py
+++ fcg/config/voxsim/world.py
@@ -9,14 +9,6 @@ class World:
     :func:`simulator.factory.geometry_factory.handlers.geometry_handler.GeometryHandler`.
 
     See :meth:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory.get_geometry_handler`
-
-    Attributes
-    ----------
-    resolution : :obj:`fcg.typing.Vec3i`
-        See :meth:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory.get_geometry_handler`
-    spacing : :obj:`fcg.typing.Vec3i`
-        See :meth:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory.get_geometry_handler`
-
     """
 
     resolution: fcg.typing.Vec3i
