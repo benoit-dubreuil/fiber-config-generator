ce2196bc73fd80d21e9e085497a6024c977c1dbd
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-06-26 18:13:53 -0400

    WIP added ClusterParams

--- fcg/voxsim/params/cluster.py
+++ fcg/voxsim/params/cluster.py
@@ -1,1 +1,21 @@
-# TODO
+import dataclasses
+
+import fcg.typing
+
+
+@dataclasses.dataclass(frozen=True)
+class ClusterParams:
+    """White fiber configuration generation parameters wrapper of
+    :meth:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory.create_cluster.
+
+    Attributes
+    ----------
+    world_center : fcg.typing.Vec3i
+        See the parameter :python:`world_center` of
+        :meth:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory.create_cluster
+    """
+
+    world_center: fcg.typing.Vec3f = (0.5, 0.5, 0.5)
+    "See :obj:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory.create_cluster.world_center"
+
+    # TODO
