be629000b409b47224805cf505276b7b491c0e0f
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-06-26 15:07:16 -0400

    Added default values to World

--- fcg/voxsim/params/world.py
+++ fcg/voxsim/params/world.py
@@ -9,9 +9,9 @@ class WorldParams:
     :meth:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory.get_geometry_handler.
     """
 
-    voxel_resolution: fcg.typing.Vec3i
+    voxel_resolution: fcg.typing.Vec3i = (1, 1, 1)
     "See :obj:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory.get_geometry_handler.resolution"
 
     # TODO : Necessary? If not, then remove and rename `voxel_resolution` to `resolution`.
-    voxel_dimensions: fcg.typing.Vec3i
+    voxel_dimensions: fcg.typing.Vec3i = (1, 1, 1)
     "See :obj:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory.get_geometry_handler.spacing"
