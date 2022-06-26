2ab971dd02895b17252831a8fea428db845a1332
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-06-26 18:05:54 -0400

    Removed voxel_dimensions param and refactor

--- fcg/voxsim/params/world.py
+++ fcg/voxsim/params/world.py
@@ -7,11 +7,12 @@ import fcg.typing
 class WorldParams:
     """White fiber configuration generation parameters wrapper of
     :meth:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory.get_geometry_handler.
+
+    Attributes
+    ----------
+    voxel_resolution : fcg.typing.Vec3i
+        See the parameter :python:`resolution` of
+        :meth:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory.get_geometry_handler
     """
 
     voxel_resolution: fcg.typing.Vec3i = (1, 1, 1)
-    "See :obj:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory.get_geometry_handler.resolution"
-
-    # TODO : Necessary? If not, then remove and rename `voxel_resolution` to `resolution`.
-    voxel_dimensions: fcg.typing.Vec3i = (1, 1, 1)
-    "See :obj:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory.get_geometry_handler.spacing"
