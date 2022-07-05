475af15ac7f9830a385dc08aeb0aba8143af4b8f
benoit-dubreuil <6900372+benoit-dubreuil@users.noreply.github.com>
2022-07-05 17:24:09 -0400

    No need for os

--- scripts/simulation_runner.py
+++ scripts/simulation_runner.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import argparse
-import os
 import pathlib
 import tempfile
 import typing
@@ -14,7 +13,7 @@ DEFAULT_SINGULARITY_NAME: typing.Final[str] = "voxsim_singularity_latest.sif"
 
 
 # TODO : Use new runner instead of legacy one.
-def run_simulation(output_folder):
+def run_simulation(output_folder: pathlib.Path):
     geometry_parameters = generate_voxsim_geom_params(output_folder, "runner_test_geometry")
 
     simulation_parameters = get_simulation_parameters(output_folder, "runner_test_simulation")
@@ -30,7 +29,7 @@ def run_simulation(output_folder):
 
     simulation_parameters = get_simulation_parameters(output_folder, "runner_test_simulation_standalone")
 
-    standalone_output = os.path.join(output_folder, "standalone_test")
+    standalone_output = output_folder / "standalone_test"
 
     runner.run_simulation_standalone(standalone_output, output_folder, simulation_parameters, "standalone")
 
@@ -38,17 +37,17 @@ def run_simulation(output_folder):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Simulation Runner Example Script")
     parser.add_argument("singularity_path", type=pathlib.Path, nargs="?", default=DEFAULT_SINGULARITY_NAME)
-    parser.add_argument("--out", type=str, required=False, help="Output directory for the files")
+    parser.add_argument("--out", type=pathlib.Path, help="Output directory for the files")
 
     args = parser.parse_args()
     args.singularity_path.resolve(strict=True)
     # TODO : pass singularity_path to simulation
 
     if args.out:
-        dest = args.out
-        os.makedirs(args.out, exist_ok=True)
+        dest: pathlib.Path = args.out
+        dest.mkdir(parents=True, exist_ok=True)
     else:
-        dest = tempfile.mkdtemp(prefix="sim_runner")
+        dest = pathlib.Path(tempfile.mkdtemp(prefix="sim_runner"))
 
     print(f"Script execution results are in : {dest}")
     run_simulation(dest)
