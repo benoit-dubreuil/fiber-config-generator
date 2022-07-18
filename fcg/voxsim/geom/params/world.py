2229b3cb897bc8f3b3de2a60fb116994331070a6
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-07-18 15:18:54 -0400

    Fixed world's doc

--- fcg/voxsim/geom/params/world.py
+++ fcg/voxsim/geom/params/world.py
@@ -12,7 +12,7 @@ class WorldParams:
     ----------
     resolution
         See the parameter :python:`resolution` of
-        :meth:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory.get_geometry_handler
+        :meth:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory.get_geometry_handler`
     """
 
     resolution: fcg.typing.Vec3i = (1, 1, 1)
