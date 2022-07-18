e1a237b6c352a4c83110cb7f1bffda6c65cd307e
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-07-18 18:10:12 -0400

    Specify param type

--- fcg/voxsim/phantom/generator.py
+++ fcg/voxsim/phantom/generator.py
@@ -12,7 +12,7 @@ def generate_fiber_tracts(
     voxsim_geom_params: _sim_geom_handlers.GeometryInfos,
     simulation_name: str = fcg.voxsim.default.SIMULATION_NAME,
     out_dir: pathlib.Path = fcg.voxsim.default.OUT_DIR,
-    singularity_conf=simulator.runner.SingularityConfig(),
+    singularity_conf: simulator.runner.SingularityConfig = simulator.runner.SingularityConfig(),
 ) -> None:
     simulation: simulator.runner.SimulationRunner = simulator.runner.SimulationRunner(singularity_conf)
 
