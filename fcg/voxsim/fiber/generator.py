77c56781797dcaca88f4e714289f54f8f17543f4
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-07-18 18:24:57 -0400

    Renamed alias

--- fcg/voxsim/fiber/generator.py
+++ fcg/voxsim/fiber/generator.py
@@ -5,7 +5,7 @@ import simulator.factory.geometry_factory.handlers as _sim_geom_handlers
 import simulator.runner
 
 import fcg.voxsim
-import fcg.voxsim.fiber as _phantom
+import fcg.voxsim.fiber as _fiber
 
 
 def generate_fiber_tracts(
@@ -20,5 +20,5 @@ def generate_fiber_tracts(
         run_name=simulation_name,
         phantom_infos=voxsim_geom_params,
         output_folder=out_dir,
-        output_nifti=_phantom.const.GENERATE_NIFTI,
+        output_nifti=_fiber.const.GENERATE_NIFTI,
     )
