e1955cee201cc18ea29ea1b015b8df993b825cb9
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-07-18 17:52:48 -0400

    Refactored imports

--- fcg/voxsim/phantom/generator.py
+++ fcg/voxsim/phantom/generator.py
@@ -3,16 +3,16 @@ import pathlib
 
 import fcg.voxsim
 import fcg.voxsim.phantom as _phantom
-from simulator.factory.geometry_factory.handlers import GeometryInfos
-from simulator.runner import SimulationRunner
+import simulator.factory.geometry_factory.handlers as _sim_geom_handlers
+import simulator.runner
 
 
 def generate_fiber_tracts(
-    voxsim_geom_params: GeometryInfos,
+    voxsim_geom_params: _sim_geom_handlers.GeometryInfos,
     out_dir: pathlib.Path = fcg.voxsim.default.OUT_DIR
 ) -> None:
     # TODO : Supply SingularityConfig to SimulationRunner with custom attribute values
-    simulation: SimulationRunner = SimulationRunner()
+    simulation: simulator.runner.SimulationRunner = simulator.runner.SimulationRunner()
 
     simulation.generate_phantom(fcg.voxsim.default.SIMULATION_NAME,
                                 voxsim_geom_params,
