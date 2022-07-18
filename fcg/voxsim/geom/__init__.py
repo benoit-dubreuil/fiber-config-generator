e85f0a2fb0767d1327668030d116f08889379fca
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-07-18 14:49:25 -0400

    geom pkg import all

--- fcg/voxsim/geom/__init__.py
+++ fcg/voxsim/geom/__init__.py
@@ -1,3 +1,6 @@
 """White fiber configuration generation parameters wrappers of
 :class:`simulator.factory.geometry_factory.geometry_factory.GeometryFactory`.
 """
+
+import params
+from .generator import generate_voxsim_geom_params
