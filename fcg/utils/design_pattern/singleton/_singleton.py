a4fec76c8367e4460d9449d7bbdef8f940bc22ee
benoit-dubreuil <6900372+benoit-dubreuil@users.noreply.github.com>
2022-07-19 18:21:23 -0400

    Make sure a singleton is only instantiated once

--- fcg/utils/design_pattern/singleton/_singleton.py
+++ fcg/utils/design_pattern/singleton/_singleton.py
@@ -7,6 +7,12 @@ _T = typing.TypeVar("_T", bound="Singleton")
 class Singleton(metaclass=abc.ABCMeta):
     __singleton: typing.ClassVar[typing.Optional[_T]] = None
 
+    def __new__(cls, *args, **kwargs):
+        if cls.__singleton is not None:
+            raise TypeError("A singleton cannot be instantiated more than once")
+
+        return super().__new__(cls, *args, **kwargs)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
