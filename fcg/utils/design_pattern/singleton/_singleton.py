8739ecfdadaf20daea67c76de269a0b0bfc1b024
benoit-dubreuil <6900372+benoit-dubreuil@users.noreply.github.com>
2022-07-19 19:26:28 -0400

    WIP

--- fcg/utils/design_pattern/singleton/_singleton.py
+++ fcg/utils/design_pattern/singleton/_singleton.py
@@ -1,5 +1,7 @@
 import abc
 import typing
+import fcg.utils.introspection.caller
+import fcg.utils.introspection as _intro
 
 _T = typing.TypeVar("_T", bound="Singleton")
 
@@ -10,6 +12,16 @@ class Singleton(metaclass=abc.ABCMeta):
     def __new__(cls, *args, **kwargs):
         if cls.__singleton is not None:
             raise TypeError("A singleton cannot be instantiated more than once")
+
+        caller = _intro.caller.get_caller()
+
+        if hasattr(caller.f_locals, "cls"):
+            caller_cls = getattr(caller.f_locals, "cls")
+
+            if hasattr(caller_cls, "get_singleton"):
+                ...
+        if _intro.caller.get_caller() is not cls.get_singleton:
+            raise TypeError("A singleton can only be created implicitly by its special getter")
 
         return super().__new__(cls, *args, **kwargs)
 
@@ -22,3 +34,19 @@ class Singleton(metaclass=abc.ABCMeta):
             cls.__singleton = cls()
 
         return cls.__singleton
+
+
+class Bob(Singleton):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+bob = Bob()
+print(bob)
+
+bob = Bob.get_singleton()
+print(bob)
+
+bob = Bob()
+print(bob)
