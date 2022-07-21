b0db57e8ef5c0d43fc10eeb698a4e3170b61b785
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-07-21 11:51:18 -0400

    WIP : app

--- fcg/app/_app.py
+++ fcg/app/_app.py
@@ -2,8 +2,20 @@ import abc
 
 
 class App(metaclass=abc.ABCMeta):
+    _is_running: bool = False
+
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-# TODO
-#   def init(self):
+    def start(self) -> None:
+        # TODO
+        ...
+        self._is_running = True
+
+    def shutdown(self) -> None:
+        # TODO
+        ...
+
+    @property
+    def is_running(self) -> bool:
+        return self._is_running
