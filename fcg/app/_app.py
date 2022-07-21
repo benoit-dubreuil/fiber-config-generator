7df3f5ca327d05c3d0f9e3a0492fc0714422a8cf
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-07-21 15:08:47 -0400

    Added shutdown integrity property

--- fcg/app/_app.py
+++ fcg/app/_app.py
@@ -3,20 +3,27 @@ import abc
 
 class App(metaclass=abc.ABCMeta):
     _is_running: bool = False
+    _has_correctly_shutdown: bool = True
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
     def start(self) -> None:
         # TODO
-        ...
         self._is_running = True
 
     def shutdown(self) -> None:
+        self._has_correctly_shutdown = False
         self._is_running = False
+
         # TODO
-        ...
+
+        self._has_correctly_shutdown = True
 
     @property
     def is_running(self) -> bool:
         return self._is_running
+
+    @property
+    def has_correctly_shutdown(self) -> bool:
+        return self._has_correctly_shutdown
