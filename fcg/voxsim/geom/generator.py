d75a20a3b680b19af3a6aba676fdfde357afa068
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-08-14 12:23:10 -0400

    Fix cyclic dependency : fcg.voxsim.geom and fcg.voxsim.geom.generator.py

--- fcg/voxsim/geom/generator.py
+++ fcg/voxsim/geom/generator.py
@@ -6,12 +6,14 @@ import simulator.factory as _sim_factory
 import simulator.factory.geometry_factory.handlers as _sim_geom_handlers
 
 import fcg.voxsim
-import fcg.voxsim.geom as _geom
+from . import const as _const
+from . import default as _default
+from . import params as _params
 
 
 def generate_voxsim_geom_params(
     root_out_dir: pathlib.Path = fcg.voxsim.default.ROOT_OUT_DIR,
-    out_files_prefix: str = _geom.default.OUT_GEOM_FILES_PREFIX,
+    out_files_prefix: str = _default.OUT_GEOM_FILES_PREFIX,
 ) -> _sim_geom_handlers.GeometryInfos:
     """
     Generates the VoxSim (through Simulation Generator) geometry parameters configuration files.
@@ -31,27 +33,27 @@ def generate_voxsim_geom_params(
 
     """
     geometry_handler: _sim_geom_handlers.GeometryHandler = _sim_factory.GeometryFactory.get_geometry_handler(
-        _geom.const.MRI_RESOLUTION, _geom.const.MRI_VOXEL_SPACING
+        _const.MRI_RESOLUTION, _const.MRI_VOXEL_SPACING
     )
 
     # TODO : Customise
     bundle1 = _sim_factory.GeometryFactory.create_bundle(
-        _geom.params.default.BUNDLE_RADIUS,
-        _geom.params.default.BUNDLE_SYMMETRY,
-        _geom.params.default.N_POINT_PER_CENTROID,
-        _geom.params.default.BASE_ANCHORS,
+        _params.default.BUNDLE_RADIUS,
+        _params.default.BUNDLE_SYMMETRY,
+        _params.default.N_POINT_PER_CENTROID,
+        _params.default.BASE_ANCHORS,
     )
 
     cluster = _sim_factory.GeometryFactory.create_cluster(
         _sim_factory.GeometryFactory.create_cluster_meta(
-            _geom.const.DIMENSIONALITY,
-            _geom.params.default.BUNDLE_N_FIBERS,
-            _geom.const.SAMPLING_DISTANCE,
-            _geom.params.default.BUNDLE_CENTER,
-            _geom.params.default.BUNDLE_LIMITS,
+            _const.DIMENSIONALITY,
+            _params.default.BUNDLE_N_FIBERS,
+            _const.SAMPLING_DISTANCE,
+            _params.default.BUNDLE_CENTER,
+            _params.default.BUNDLE_LIMITS,
         ),
         [bundle1],
-        _geom.params.default.WORLD_CENTER,
+        _params.default.WORLD_CENTER,
     )
 
     geometry_handler.add_cluster(cluster)
