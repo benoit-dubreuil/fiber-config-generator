8c07b4c4489a52c7edb0fed3b74d65fe17245bc8
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-08-15 14:51:00 -0400

    Improved bundle doc

--- fcg/voxsim/geom/param/bundle/_bundle.py
+++ fcg/voxsim/geom/param/bundle/_bundle.py
@@ -5,7 +5,7 @@ import dataclasses
 class BundleParams:
     """The general generation parameters of a fiber bundle.
 
-    Abstract white fiber configuration generation parameters wrapper of
+    Base white fiber configuration generation parameters wrapper of
     :meth:`simulator.factory.geometry_factory.GeometryFactory.create_bundle`.
 
     Attributes
