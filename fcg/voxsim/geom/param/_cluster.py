1e6844debede3433804336cbb2932911f6be3ce1
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-08-16 12:04:25 -0400

    More explicit doc

--- fcg/voxsim/geom/param/_cluster.py
+++ fcg/voxsim/geom/param/_cluster.py
@@ -7,13 +7,13 @@ from . import default as _default
 
 @dataclasses.dataclass()
 class ClusterParams:
-    """White fiber configuration generation parameters wrapper of
+    """White fiber configuration generation parameters wrapper of the method
     :meth:`simulator.factory.geometry_factory.GeometryFactory.create_cluster`.
 
     Attributes
     ----------
     world_center
-        See the parameter :obj:`world_center` of
+        See the parameter :obj:`world_center` of the method
         :meth:`simulator.factory.geometry_factory.GeometryFactory.create_cluster`.
 
     """
