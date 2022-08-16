3b9c8a8626c7aa7eedcb88c956212b54d877d40a
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-08-16 15:56:11 -0400

    Transformed BundleParamsBuilder in a dataclass

--- fcg/voxsim/geom/param/bundle_builder/_builder.py
+++ fcg/voxsim/geom/param/bundle_builder/_builder.py
@@ -1,14 +1,35 @@
 import abc
+import dataclasses
 import typing
 
 import fcg.typing
 from .._bundle import BundleParams
 
 
+@dataclasses.dataclass
 class BundleParamsBuilder(metaclass=abc.ABCMeta):
-    __radius: float
-    __symmetry: float
-    __n_point_per_centroid: int
+    """Builder of fiber bundles generation parameters.
+
+    This class builds :class:`fcg.voxsim.geom.param.BundleParams`.
+
+    Attributes
+    ----------
+    radius
+        See the parameter :obj:`radius` of the method
+        :meth:`simulator.factory.geometry_factory.GeometryFactory.create_bundle`.
+    symmetry
+        See the parameter :obj:`symmetry` of the method
+        :meth:`simulator.factory.geometry_factory.GeometryFactory.create_bundle`.
+        See the definition of a cross-section : https://en.wikipedia.org/wiki/Cross_section_(geometry).
+    n_point_per_centroid
+        See the parameter :obj:`n_point_per_centroid` of the method
+        :meth:`simulator.factory.geometry_factory.GeometryFactory.create_bundle`.
+    """
+
+    # TODO : Default values
+    radius: float
+    symmetry: float
+    n_point_per_centroid: int
 
     @abc.abstractmethod
     def build(self, *args, **kwargs) -> BundleParams:
@@ -18,27 +39,3 @@ class BundleParamsBuilder(metaclass=abc.ABCMeta):
     def _build_bundle(self, anchors: list[fcg.typing.Vec3f]) -> BundleParams:
         return BundleParams(radius=radius, symmetry=symmetry, n_point_per_centroid=n_point_per_centroid,
                             anchors=anchors)
-
-    @property
-    def radius(self) -> float:
-        return self.__radius
-
-    @radius.setter
-    def radius(self, new_value: float) -> None:
-        self.__radius = new_value
-
-    @property
-    def symmetry(self) -> float:
-        return self.__symmetry
-
-    @symmetry.setter
-    def symmetry(self, new_value: float) -> None:
-        self.__symmetry = new_value
-
-    @property
-    def n_point_per_centroid(self) -> int:
-        return self.__n_point_per_centroid
-
-    @n_point_per_centroid.setter
-    def n_point_per_centroid(self, new_value: int) -> None:
-        self.n_point_per_centroid = new_value
