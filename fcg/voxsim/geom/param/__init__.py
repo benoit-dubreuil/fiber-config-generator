586d5a6fb169223317b031e1d9ac5f6fdfde9a35
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-08-16 17:34:28 -0400

    Revert "Simulation Generator's world is already abstracted with constants"
    This reverts commit f39ed8e3bf87b46befd586f652117c49f97e7ae3.

--- fcg/voxsim/geom/param/__init__.py
+++ fcg/voxsim/geom/param/__init__.py
@@ -2,5 +2,6 @@ from . import default
 
 from ._bundle import BundleParams
 from ._cluster import ClusterParams
+from ._world import WorldParams
 
 from .bundle_builder import *
---
+++ fcg/voxsim/geom/param/_world.py
@@ -0,0 +1,14 @@
+import dataclasses
+
+import fcg.typing
+
+
+@dataclasses.dataclass()
+class WorldParams:
+    """White fiber configuration generation parameters wrapper of
+    :meth:`simulator.factory.geometry_factory.GeometryFactory.get_geometry_handler`.
+
+    Attributes
+    ----------
+
+    """
