d7df45b8c985358fa282215274615c088b3ff378
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-08-16 17:22:29 -0400

    Improved doc of StraightBundleParamsBuilder

--- fcg/voxsim/geom/param/bundle_builder/_straight.py
+++ fcg/voxsim/geom/param/bundle_builder/_straight.py
@@ -6,7 +6,8 @@ class StraightBundleParamsBuilder(BundleParamsBuilder):
     """Builder of straight fiber bundles generation parameters.
 
     This class builds :class:`fcg.voxsim.geom.param.BundleParams` which represent straight fiber bundles. A straight
-    fiber bundle forms a straight line.
+    fiber bundle forms a straight line. This is why the attribute :attr:`.n_point_per_centroid` is minimized,
+    as there is no need to add more details to this simple geometric shape.
     """
 
     def build(self, radius: float, symmetry: float, n_point_per_centroid: int) -> BundleParams:
