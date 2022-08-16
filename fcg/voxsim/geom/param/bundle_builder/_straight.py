8b5eec385d027ac0ffb100adde829b1b3e30dac5
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-08-16 17:23:03 -0400

    Made StraightBundleParamsBuilder a dataclass and override its attribute

--- fcg/voxsim/geom/param/bundle_builder/_straight.py
+++ fcg/voxsim/geom/param/bundle_builder/_straight.py
@@ -1,7 +1,10 @@
+import dataclasses
+
 from ._builder import BundleParamsBuilder
 from .._bundle import BundleParams
 
 
+@dataclasses.dataclass
 class StraightBundleParamsBuilder(BundleParamsBuilder):
     """Builder of straight fiber bundles generation parameters.
 
@@ -9,6 +12,8 @@ class StraightBundleParamsBuilder(BundleParamsBuilder):
     fiber bundle forms a straight line. This is why the attribute :attr:`.n_point_per_centroid` is minimized,
     as there is no need to add more details to this simple geometric shape.
     """
+
+    n_point_per_centroid: int = 1
 
     def build(self, radius: float, symmetry: float, n_point_per_centroid: int) -> BundleParams:
         # TODO
