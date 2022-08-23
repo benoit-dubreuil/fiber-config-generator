a2fa9bcf27cf920ce14da63302bb9a4abb5233a1
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-08-23 12:15:27 -0400

    Made BUNDLE_CENTER dependent on BUNDLE_LIMITS

--- fcg/voxsim/geom/param/default.py
+++ fcg/voxsim/geom/param/default.py
@@ -5,8 +5,12 @@ import fcg.typing
 from .. import const as _geom_const
 
 BUNDLE_N_FIBERS: typing.Final[int] = 800
-BUNDLE_CENTER: typing.Final[fcg.typing.Vec3f] = (0.5, 0.5, 0.5)
 BUNDLE_LIMITS: typing.Final[list[list[float]]] = [[0, 1], [0, 1], [0, 1]]
+BUNDLE_CENTER: typing.Final[fcg.typing.Vec3f] = (
+    (BUNDLE_LIMITS[0][0] + BUNDLE_LIMITS[0][1]) / 2,
+    (BUNDLE_LIMITS[1][0] + BUNDLE_LIMITS[1][1]) / 2,
+    (BUNDLE_LIMITS[2][0] + BUNDLE_LIMITS[2][1]) / 2,
+)
 
 CLUSTER_WORLD_POSITION: typing.Final[fcg.typing.Vec3f] = BUNDLE_CENTER
 
