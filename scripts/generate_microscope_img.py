46f50dae5f68331491ae662fe202b9aed6c0afb0
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-08-29 18:59:09 -0400

    Added PSF input

--- scripts/generate_microscope_img.py
+++ scripts/generate_microscope_img.py
@@ -8,6 +8,7 @@ import colorama
 
 import fcg.app
 
+_DEFAULT_PSF_PATH: typing.Final[pathlib.Path] = pathlib.Path("psf.tif")
 _DEFAULT_OUT_PATH: typing.Final[pathlib.Path] = pathlib.Path("out.tiff")
 
 
@@ -19,16 +20,21 @@ class GenerateMicroscopeImg(fcg.app.App):
 
     def _exec_logic(self) -> None:
         parser = argparse.ArgumentParser("Generate a 3D microscope image representation from fiber bundles")
+        parser.add_argument("psf", type=pathlib.Path, default=_DEFAULT_PSF_PATH,
+                            help="The input path to the PSF. Ex: \"./generate_fiber_config.py psf.tif\"")
         parser.add_argument(
             "--out", type=pathlib.Path, default=_DEFAULT_OUT_PATH, help="The output path with the complete filename."
         )
 
         args = parser.parse_args()
 
+        psf: pathlib.Path = args.psf
+        psf = psf.resolve(strict=True)
+
         out: pathlib.Path = args.out
         out_dir = out.parent
         out_dir.mkdir(parents=True, exist_ok=True)
-        out = out.resolve(strict=True)
+        out_dir = out_dir.resolve(strict=True)
 
 
 if __name__ == "__main__":
