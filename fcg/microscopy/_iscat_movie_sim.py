0a57ad5e3f8c140b19751c95adc61f69a8f4ed48
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-09-05 14:22:53 -0400

    Trying to fix isort E302

--- fcg/microscopy/_iscat_movie_sim.py
+++ fcg/microscopy/_iscat_movie_sim.py
@@ -10,9 +10,9 @@ import tqdm
 from ._psf import load_psf
 from ._tract import Tracts, load_tracts
 
+
 # TODO : Migrate imageio v2 API to v3 API :
 #  See https://imageio.readthedocs.io/en/stable/reference/userapi.html#migrating-to-the-v3-api
-
 # Moving Acquisition simulation
 #
 # @formatter:off
